3839d641b39fc7258b4a5df191e8871773c3b506
RIbbet <70328244+Ribbet400@users.noreply.github.com>
2020-09-13 14:09:48 +0100

    Leaving, more messages

--- Bot1.py
+++ Bot1.py
@@ -1,20 +1,19 @@
-
+# BOTTTTTTTTTTT
+# CREATED BY RIBBET400
 import discord
+import random
+from discord.ext import commands
 
-INFO = open('FILE PATH','r').read()
+INFO = open('c:/Users/jackl/OneDrive/Documents/Coding/INFO.txt','r').read()
 INFO = INFO.split('/')
 TOKEN = INFO[0]
 GUILD = INFO[1]
 
 client = discord.Client()
 
-
-
-
-
 @client.event
-async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('BOT'))
+async def on_ready(): # WHEN CONNECTED
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('4D chess'))
     print(f'{client.user.name} has connected to Discord!')
     for guild in client.guilds:
         if guild.name == GUILD:
@@ -29,10 +28,36 @@ async def on_ready():
     print(f'Guild Members:\n - {members}')
 
 @client.event
-async def on_member_join(member):
+async def on_message(message): # RESPONDS TO MESSAGES
+    if message.author == client.user: # PREVENTS LOOP
+        return
+
+    Greetings = ['Bonjour','Hola','Hi','Salutations','Greetings','Hello']
+    if message.content.lower() == 'hi' or message.content.lower() == 'hello':
+        response = random.choice(Greetings)
+        await message.channel.send(response)
+
+    if message.content.lower() == 'rickroll':
+        await message.channel.send('https://www.youtube.com/watch?v=dQw4w9WgXcQ&feature=youtu.be')
+
+    if message.content.lower() == 'github':
+        await message.channel.send('https://github.com/Ribbet400')
+        
+
+
+@client.event
+async def on_member_join(member): # WHEN MEMBERS JOIN
     await member.create_dm()
     await member.dm_channel.send(
-        f'Bonjour {member.name}, bienvenue sur mon serveur!'
+        f'Bonjour {member.name}, bienvenue sur mon serveur! :thumbsup:'
     )
+
+@client.event
+async def on_member_remove(member): # WHEN MEMBERS LEAVE
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Au revoir {member.name} :thumbsup:'
+    )
+
 
 client.run(TOKEN)
